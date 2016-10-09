7f18fb316355a039eca85454711da810ad824af3
owen-carter <owenjiao@outlook.com>
2016-10-09 13:42:50 +0800

    init

--- control/index.py
+++ control/index.py
@@ -5,16 +5,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    DB_CONNECT_STRING = 'sqlite:///../data/user.db'
-    engine = create_engine(DB_CONNECT_STRING, echo = True)
-    dbsession = sessionmaker(bind = engine)
-    print dbsession
-    user = User(name = 'jft', password = 'jft')
-    dbsession.add(user)
-    dbsession.commit()
-    query = dbsession.query(User)
-    # return render_template('index.html')
-    return query.all()
+    # DB_CONNECT_STRING = 'sqlite:///../data/user.db'
+    # engine = create_engine(DB_CONNECT_STRING, echo = True)
+    # dbsession = sessionmaker(bind = engine)
+    # print dbsession
+    # user = User(name = 'jft', password = 'jft')
+    # dbsession.add(user)
+    # dbsession.commit()
+    # query = dbsession.query(User)
+    return render_template('index.html')
+    # return query.all()
     # return 'spec'
