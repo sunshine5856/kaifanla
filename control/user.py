e9ac3e3183a162572abe1ccfdd43b6d403ecbd6a
owen-carter <owenjiao@outlook.com>
2016-10-09 21:13:39 +0800

    user ok

--- control/user.py
+++ control/user.py
@@ -10,6 +10,11 @@ def show_user_profile(username):
     # use cookies.get(key) instead of cookies[key] to not get a
     # KeyError if the cookie is missing.
     # show the user profile for that user
+    # show the user profile for that user
+    # show the user profile for that user
+    # show the user profile for that user
+    # show the user profile for that user
+    # show the user profile for that user
     return 'User %s' % username
     # resp = make_response(render_template(...))
     resp.set_cookie('username', 'the username')
